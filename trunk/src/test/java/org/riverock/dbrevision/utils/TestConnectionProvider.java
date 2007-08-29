11abf44367ec9ea4917ec614b0e29857a24dfdb9
serg_main <serg_main@a94bdbc2-7502-4dfa-8a05-00539c9f375a>
2007-08-29 21:30:18 +0000

    Updated number of current version

---
+++ trunk/src/test/java/org/riverock/dbrevision/utils/TestConnectionProvider.java
@@ -0,0 +1,34 @@
+package org.riverock.dbrevision.utils;
+
+import org.riverock.dbrevision.db.Database;
+import org.riverock.dbrevision.db.DatabaseFactory;
+import org.riverock.dbrevision.db.DatabaseStructureManager;
+import org.riverock.dbrevision.annotation.schema.db.DbTable;
+import org.riverock.dbrevision.Constants;
+
+import java.sql.*;
+import java.util.List;
+
+/**
+ * User: SergeMaslyukov
+ * Date: 29.08.2007
+ * Time: 23:55:28
+ */
+public class TestConnectionProvider {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:hsqldb:hsql://localhost:9001";
+
+        Class.forName("org.hsqldb.jdbcDriver");
+
+        Connection conn = DriverManager.getConnection(url, "sa", null);
+
+        Database database = DatabaseFactory.getInstance(conn, Database.Family.HYPERSONIC);
+
+        DatabaseMetaData metaData = database.getConnection().getMetaData();
+//        String dbSchema = metaData.getUserName();
+        String dbSchema = "";
+        List<DbTable> list = DatabaseStructureManager.getTableList(database.getConnection(), dbSchema, Constants.DB_REVISION_TABLE_NAME);
+
+        System.out.println("list = " + list);
+    }
+}
