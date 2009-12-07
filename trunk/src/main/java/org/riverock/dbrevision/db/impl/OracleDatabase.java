888ee87b61465d45750f69decb02af7cd0db3e6b
serg_main <serg_main@a94bdbc2-7502-4dfa-8a05-00539c9f375a>
2009-12-07 17:53:57 +0000

    License changed to "Apache License 2.0" type

--- trunk/src/main/java/org/riverock/dbrevision/db/impl/OracleDatabase.java
+++ trunk/src/main/java/org/riverock/dbrevision/db/impl/OracleDatabase.java
@@ -1,27 +1,17 @@
 /*
- * org.riverock.dbrevision - Database revision engine
- * For more information about DbRevision, please visit project site
- * http://www.riverock.org
+ * Copyright 2007 Sergei Maslyukov at riverock.org
  *
- * Copyright (C) 2006-2006, Riverock Software, All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Riverock - The Open-source Java Development Community
- * http://www.riverock.org
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.riverock.dbrevision.db.impl;
 
@@ -61,7 +51,7 @@ import org.riverock.dbrevision.exception.TableNotFoundException;
 import org.riverock.dbrevision.utils.DbUtils;
 
 /**
- * Класс OracleDatabase прденазначен для коннекта к оракловской базе данных.
+ * пїЅпїЅпїЅпїЅпїЅ OracleDatabase пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
  * <p/>
  * $Id: OracleDatabase.java 1141 2006-12-14 14:43:29Z serg_main $
  */
